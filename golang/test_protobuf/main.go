5432ab1659a050707e9e9c3d4dd1ac10e13dfcfc
pwong00710 <peterwong@Peterde-MacBook-Pro.local>
2017-07-03 19:06:02 +0800

    updates

---
+++ golang/test_protobuf/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "log"
+
+  "github.com/golang/protobuf/proto"
+)
+
+func main() {
+
+  test := &Test{
+      Label: proto.String("hello"),
+      Type:  proto.Int32(17),
+      Optionalgroup: &Test_OptionalGroup{
+          RequiredField: proto.String("good bye"),
+      },
+  }
+  data, err := proto.Marshal(test)
+  if err != nil {
+      log.Fatal("marshaling error: ", err)
+  }
+  newTest := &Test{}
+  err = proto.Unmarshal(data, newTest)
+  if err != nil {
+      log.Fatal("unmarshaling error: ", err)
+  }
+  // Now test and newTest contain the same data.
+  if test.GetLabel() != newTest.GetLabel() {
+      log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
+  }
+
+  log.Printf("Unmarshalled to: %+v", newTest)
+
+}
